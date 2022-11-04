643a831c41e63016f256bca4510ecaf63ed17fd1
GacAcassio <acassiocorreia@gmail.com>
2022-11-04 01:37:15 +0000

    test5

--- CodeChef/testgit/c.c
+++ CodeChef/testgit/c.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 int main(){
     printf("Hellho world, i am testing git om my new PC");
-    printf("New Comit");
-    printf("Do a new Comit");
-    printf("new commit test 4");
+    printf("new commit test 5");
     return 0;
 }
